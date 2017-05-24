b21d88b7292d45216a6204ab8986660126004b55
yoo2 <yoo2yoo2yoo2@gmail.com>
2017-05-24 20:08:28 +0900

    code 추가

--- init.sql
+++ init.sql
@@ -9,6 +9,7 @@ create table item(
     id int not null auto_increment,
     chat_id varchar(100) not null,
     name varchar(100) not null,
+    code varchar(100) not null,
     price int not null,
     kind boolean not null,
     primary key(id)
