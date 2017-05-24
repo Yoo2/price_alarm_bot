07a8399a0ea17b11524bd78d72e0261678d08258
yoo2 <yoo2yoo2yoo2@gmail.com>
2017-05-24 15:03:34 +0900

    sql 파일 추가

---
+++ init.sql
@@ -0,0 +1,15 @@
+create database price_alarm_bot;
+
+create user 'tele'@'localhost' identified by 'xpffp';
+grant all privileges on price_alarm_bot.* to tele@'localhost';
+
+use price_alarm_bot;
+
+create table item(
+    id int not null auto_increment,
+    chat_id varchar(100) not null,
+    name varchar(100) not null,
+    price int not null,
+    kind boolean not null,
+    primary key(id)
+);
